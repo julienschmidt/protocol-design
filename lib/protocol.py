801f3ea049851f12e3046b74e202a10c13125fec
Julien Schmidt <julien.schmidt@tum.de>
2018-05-08 21:41:07 +0200

    implement remaining packet send methods

--- lib/protocol.py
+++ lib/protocol.py
@@ -1,9 +1,12 @@
 import asyncio
+import stat
 
 from . import packettype
 from . import sha256
 
+
 class BaseCsyncProtocol(asyncio.DatagramProtocol):
+
     def __init__(self):
         self.transport = None
 
@@ -76,7 +79,6 @@ class BaseCsyncProtocol(asyncio.DatagramProtocol):
     def handle_invalid_packet(self, data, addr):
         print('dropping invalid packet from {}'.format(addr))
 
-
     def send_client_hello(self, client_id, addr=None):
         data = packettype.Client_Hello + client_id.to_bytes(8, byteorder='big')
         return self.sendto(data, addr)
@@ -90,13 +92,63 @@ class BaseCsyncProtocol(asyncio.DatagramProtocol):
             print(len(filename), filename, sha256.hex(filehash))
         return self.sendto(data, addr)
 
+    def send_file_metadata(self, filehash, filename, statinfo, addr=None):
+        data = (packettype.File_Metadata +
+                filehash +
+                (len(filename)).to_bytes(2, byteorder='big') +
+                filename +
+                statinfo[stat.ST_SIZE].to_bytes(8, byteorder='big') +
+                (statinfo[stat.ST_MODE] & 0o777).to_bytes(2, byteorder='big') +
+                statinfo[stat.ST_MTIME].to_bytes(4, byteorder='big'))
+        return self.sendto(data, addr)
+
+    def send_ack_metadata(self, filehash, filename, upload_id, resume_at_byte=0, addr=None):
+        resume = bytes()
+        if resume_at_byte > 0:
+            resume = resume_at_byte.to_bytes(8, byteorder='big')
+        data = (packettype.Ack_Metadata +
+                filehash +
+                (len(filename)).to_bytes(2, byteorder='big') +
+                filename +
+                upload_id.to_bytes(4, byteorder='big') +
+                resume)
+        return self.sendto(data, addr)
+
+    def send_file_upload(self, upload_id, start_byte, payload, addr=None):
+        data = (packettype.File_Upload +
+                upload_id.to_bytes(4, byteorder='big') +
+                start_byte.to_bytes(8, byteorder='big') +
+                (len(payload)).to_bytes(2, byteorder='big') +
+                payload)
+        return self.sendto(data, addr)
+
+    def send_ack_upload(self, upload_id, acked_bytes, addr=None):
+        data = (packettype.Ack_Upload +
+                upload_id.to_bytes(4, byteorder='big') +
+                acked_bytes.to_bytes(8, byteorder='big'))
+        return self.sendto(data, addr)
+
+    def send_file_delete(self, filehash, filename, addr=None):
+        data = (packettype.File_Delete +
+                filehash +
+                (len(filename)).to_bytes(2, byteorder='big') +
+                filename)
+        return self.sendto(data, addr)
+
+    def send_ack_delete(self, filehash, filename, addr=None):
+        data = (packettype.Ack_Delete +
+                filehash +
+                (len(filename)).to_bytes(2, byteorder='big') +
+                filename)
+        return self.sendto(data, addr)
+
     # first byte must be the packet type
     def sendto(self, data, addr=None):
         # add packet hash
-        h = sha256.hash(data)
-        print(sha256.hex(h))
+        packethash = sha256.hash(data)
+        print(sha256.hex(packethash))
 
-        data = h + data
+        data = packethash + data
         #print("Sending:", len(data), data)
         self.transport.sendto(data, addr)
         return len(data)
