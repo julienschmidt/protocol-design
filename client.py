6a80447e3dc443bc6c291764dd0e417891bd676f
Julien Schmidt <julien.schmidt@tum.de>
2018-05-08 11:14:00 +0200

    integrate file watcher in client

--- client.py
+++ client.py
@@ -4,16 +4,51 @@ import os
 import random
 import signal
 import sys
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 from lib import packettype
 from lib import sha256
 from lib.protocol import BaseCsyncProtocol
 
 
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self, loop, protocol):
+        self.loop = loop
+        self.protocol = protocol
+
+    def on_created(self, event):
+        if event.is_directory:
+            return
+
+        self.loop.call_soon_threadsafe(self.protocol.upload_file, event.src_path)
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            return
+
+        self.loop.call_soon_threadsafe(self.protocol.delete_file, event.src_path)
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+
+        self.loop.call_soon_threadsafe(self.protocol.update_file, event.src_path)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            return
+
+        self.loop.call_soon_threadsafe(self.protocol.move_file, event.src_path, event.dest_path)
+
+
 class ClientCsyncProtocol(BaseCsyncProtocol):
-    def __init__(self, loop):
+    def __init__(self, loop, path):
         super(ClientCsyncProtocol, self).__init__();
         self.loop = loop
+        self.path = path
 
         # generate client ID
         self.id = random.getrandbits(64)
@@ -43,14 +78,40 @@ class ClientCsyncProtocol(BaseCsyncProtocol):
             data = data[32:]
         # TODO: verify len(data) == 0
 
+        # start file dir observer
+        event_handler = FileEventHandler(self.loop, self)
+        self.observer = Observer()
+        self.observer.schedule(event_handler, self.path, recursive=False)
+        self.observer.start()
+
+    def upload_file(self, path):
+        #sha256.hashFile(path)
+        print("upload", path)
+
+    def delete_file(self, path):
+        print("delete", path)
+
+    def update_file(self, path):
+        #sha256.hashFile(path)
+        print("update", path)
+
+    def move_file(self, old_path, new_path):
+        #sha256.hashFile(new_path)
+        print("move", old_path, "to", new_path)
+
+
     def connection_lost(self, exc):
         print("Socket closed, stop the event loop")
         loop = asyncio.get_event_loop()
         loop.stop()
+        if self.observer:
+            self.observer.stop()
 
     def signal(self, signame):
         print("got signal %s: exit" % signame)
         self.loop.stop()
+        if self.observer:
+            self.observer.stop()
 
 
 def run(args):
@@ -60,7 +121,7 @@ def run(args):
     server_address = (args.host, args.port)
     print(server_address)
     connect = loop.create_datagram_endpoint(
-        lambda: ClientCsyncProtocol(loop),
+        lambda: ClientCsyncProtocol(loop, args.path),
         remote_addr=server_address)
     transport, protocol = loop.run_until_complete(connect)
 
