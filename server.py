a2042a916026c3f053d4ccf9af97a15f8f23c217
Julien Schmidt <julien.schmidt@tum.de>
2018-05-11 02:22:14 +0200

    server: implement upload info handling

--- server.py
+++ server.py
@@ -5,13 +5,35 @@ Server implementation
 import asyncio
 import functools
 import os
+import random
 import signal
 import time
 
+from shutil import move
+from tempfile import mkstemp
+
 from lib import files
 from lib import sha256
-from lib.protocol import BaseCsyncProtocol
+from lib.protocol import BaseCsyncProtocol, ErrorType
 
+
+class FileUpload:
+    """
+    Data holder for file uploads.
+    """
+
+    def __init__(self, filehash, filename, size, permissions, modified_at):
+        self.filehash = filehash
+        self.filename = filename
+        self.size = size
+        self.permissions = permissions
+        self.modified_at = modified_at
+
+        # Buffered Chunks (Linked List)
+        self.buffered_chunks = list()
+
+        self.tmpfile = mkstemp()
+        self.next_byte = 0
 
 class ServerCsyncProtocol(BaseCsyncProtocol):
     """
@@ -34,6 +56,13 @@ class ServerCsyncProtocol(BaseCsyncProtocol):
             self.fileinfo[file.encode('utf8')] = filehash
         print('\n')
 
+        # maps upload IDs to in-progress file uploads
+        self.uploads = dict()
+
+        # maps filenames to assigned upload IDs, used to check for conflicts and
+        # to resume uploads
+        self.active_uploads = dict()
+
     def handle_client_hello(self, data, addr):
         print('received Client_Hello from', addr)
         valid, client_id = self.unpack_client_hello(data)
@@ -43,7 +72,6 @@ class ServerCsyncProtocol(BaseCsyncProtocol):
             return
 
         print('client wants to connect with clientID:', client_id)
-        print('sending server hallo for containing the following file info:', self.fileinfo)
 
         # respond with Server_Hello
         sent = self.send_server_hello(self.fileinfo, addr)
@@ -59,11 +87,20 @@ class ServerCsyncProtocol(BaseCsyncProtocol):
         print(sha256.hex(filehash), filename, size,
               oct(permissions), time.ctime(modified_at))
 
-        # TODO: handle size=0 (no upload necessary)
+        upload_id, start_at, error = self.init_upload(
+            filehash, filename, size, permissions, modified_at)
+        if error:
+            # TODO: send error
+            pass
 
         sent = self.send_ack_metadata(
             filehash, filename, upload_id, start_at, addr)
         print('sent {} bytes back to {}'.format(sent, addr))
+
+        print("upload size", size)
+        if size == 0:
+            # no upload necessary
+            self.finalize_upload(upload_id)
 
     def handle_file_upload(self, data, addr):
         print('received File_Upload from', addr)
@@ -79,12 +116,73 @@ class ServerCsyncProtocol(BaseCsyncProtocol):
             upload_id, payload_start_byte + len(payload), addr)
         print('sent {} bytes back to {}'.format(sent, addr))
 
+    def gen_upload_id(self):
+        """
+        Generates a unique upload ID.
+        """
+        while True:
+            upload_id = random.getrandbits(32)
+            if upload_id not in self.uploads:
+                return upload_id
+
+    def init_upload(self, filehash, filename, size, permissions, modified_at):
+        """
+        Initialize a new file upload and return the assigned upload ID.
+        """
+
+        # check for existing upload to resume
+        if filename in self.active_uploads:
+            upload_id = self.active_uploads[filename]
+            upload = self.uploads[upload_id]
+
+            if upload.filehash == filehash and upload.size == size:
+                # resume upload
+                upload.permissions = permissions
+                upload.modified_at = modified_at
+                start_at = upload.next_byte
+                return (upload_id, start_at, None)
+
+            elif upload.modified_at >= modified_at:
+                # only accept newer files
+                return (0, 0, ErrorType.Conflict)
+
+        start_at = 0
+        upload_id = self.gen_upload_id()
+        self.uploads[upload_id] = FileUpload(
+            filehash, filename, size, permissions, modified_at)
+        self.active_uploads[filename] = upload_id
+
+        return (upload_id, start_at, None)
+
+    def finalize_upload(self, upload_id):
+        """
+        Finalized file upload by moving the tmp file to the correct path and
+        applying the file permissions and modified time.
+        Afterwards it updates the servers fileinfo cache.
+        """
+
+        upload = self.uploads[upload_id]
+        del self.uploads[upload_id]
+
+        filepath = self.path+upload.filename.decode('utf8')
+        move(upload.tmpfile[1], filepath)
+        os.utime(filepath, times=(upload.modified_at, upload.modified_at))
+        os.chmod(filepath, upload.permissions)
+
+        # update cached fileinfo
+        self.fileinfo[upload.filename] = upload.filehash
+        print("finalized", upload.filename)
+
     def signal(self, signame):
         """
         UNIX signal handler.
         """
         print("got signal %s: exit" % signame)
         self.loop.stop()
+
+        # remove all temporary files
+        for upload in self.uploads.values():
+            os.remove(upload.tmpfile[1])
 
 
 def run(args):
